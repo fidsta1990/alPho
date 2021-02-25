34fe5a7e8ac3541d334e4492efef60b6daeeeab0
Fidel Collin <fidelcollin@icloud.com>
2021-02-25 18:33:58 +0000

    added styling and functionality for toggle of menu bar to show and hide nav links

--- app.js
+++ app.js
@@ -2,4 +2,13 @@
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
+// NAVBAR TOGGLE
+const toggleBtn = document.querySelector(".toggle");
+const navLinks = document.querySelector(".nav-links");
+
+toggleBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+// api call
 const url = "";
